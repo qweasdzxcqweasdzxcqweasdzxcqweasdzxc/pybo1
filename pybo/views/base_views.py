eba237ae39b3506bac669aa3abf9781a1ebce5ec
박응용 <pahkey@gmail.com>
2023-06-08 15:36:54 +0900

    logs 추가

--- pybo/views/base_views.py
+++ pybo/views/base_views.py
@@ -1,9 +1,11 @@
+import logging
+
 from django.core.paginator import Paginator
 from django.db.models import Q
 from django.shortcuts import render, get_object_or_404
 
 from pybo.models import Question
-import logging
+
 logger = logging.getLogger('pybo')
 
 
